5d0f7d3e8e57391530c74722a198af5be00ad1b9
SpreadUrWingz <scrotumtoast@gmail.com>
2024-08-09 16:10:05 -0700

    some stuff coming together

---
+++ script.js
@@ -0,0 +1,39 @@
+var playAudio ;
+var time ;
+
+function selectSound(id)
+{
+    if(id == "trumpIM")
+    {
+        playAudio = new Audio ("/audio/trumpIM.mp3") ;
+    }
+    return;
+}
+
+function stopButton(id)
+{
+    document.getElementById("button").src = "/images/buttonOFF.png";
+}
+
+function buttonPress(id, time)
+{
+    if(playAudio)
+    {
+        playAudio.pause();
+    }
+
+    selectSound(id);
+
+    playAudio.play();
+    if(playAudio)
+    {
+        document.getElementById("button").src = "/images/buttonON.png";
+    }
+    
+    setTimeout(function(){ stopButton(id); }, time);
+}
+
+function nextCard(src)
+{
+    document.getElementById("fluid").src = "https://www.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV01/SV01_EN_156.png";
+}
